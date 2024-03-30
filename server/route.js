ec2534242f99a6280c254cb40649be613d20ac84
Biswa Pradhan <127578115+lipu0052@users.noreply.github.com>
2024-03-30 14:48:09 +0000

    router

--- server/route.js
+++ server/route.js
@@ -8,27 +8,24 @@ router.get("/", (req, res) => {
   res.send("hello world");
 });
 router.post("/signup", async (req, res) => {
-  const{name , email , password} = req.body;
-  if(!name || !email || !password){
-   return res.send("please provide all the field")
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.send("please provide all the field");
   }
 
   try {
-    const userExist = await User.findOne({email});
-    if(userExist){
-     return res.status(401).json("user already exist");
-    }
-    else{
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.status(401).json("user already exist");
+    } else {
       const newUser = new User({
         name,
         email,
-        password
+        password,
       });
-       await newUser.save();
+      await newUser.save();
       res.status(201).json("register successfully");
     }
-
-    
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
@@ -37,12 +34,8 @@ router.post("/signup", async (req, res) => {
 // GET route to fetch all users
 router.get("/signin", async (req, res) => {
   try {
-    const [email,password] =req.body();
+    const [email, password] = req.body();
     const users = await User.find(email);
-
-    if(users){
-      const passCheck = await 
-    }
 
     res.json(users);
   } catch (err) {
